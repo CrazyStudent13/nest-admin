b03fd06f5cd103e03aa5dd5f29c526394b3ad8b6
crazystudent13 <1076535690@qq.com>
2025-07-08 23:10:18 +0800

    refactor(game-info): 移除游戏类型相关代码
    - 注释掉游戏信息表格中的游戏类型列
    - 注释掉查询表单中的游戏类型选择框
    - 删除了与游戏类型相关的部分代码

--- admin-vue3/src/views/game/info/index.vue
+++ admin-vue3/src/views/game/info/index.vue
@@ -4,11 +4,11 @@
       <el-form-item label="游戏名称" prop="nameZh">
         <el-input v-model="queryParams.nameZh" placeholder="请输入游戏中文名" clearable @keyup.enter="gameInfoTable.onSearch" />
       </el-form-item>
-      <el-form-item label="游戏类型" prop="gameType">
+      <!-- <el-form-item label="游戏类型" prop="gameType">
         <el-select v-model="queryParams.gameType" placeholder="请选择游戏类型" clearable style="width: 200px">
           <el-option v-for="dict in game_types" :key="dict.value" :label="dict.label" :value="dict.value" />
         </el-select>
-      </el-form-item>
+      </el-form-item> -->
       <el-form-item>
         <el-button :loading="gameInfoTable.state.loading" type="primary" icon="Search" @click="gameInfoTable.onSearch">搜索</el-button>
         <el-button :loading="gameInfoTable.state.loading" icon="Refresh" @click="gameInfoTable.onReset">重置</el-button>
@@ -29,11 +29,11 @@
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="游戏中文名" align="left" prop="nameZh" show-overflow-tooltip />
       <el-table-column label="游戏英文名" align="center" prop="nameEn" show-overflow-tooltip />
-      <el-table-column label="游戏类型" align="center" prop="gameType">
+      <!-- <el-table-column label="游戏类型" align="center" prop="gameType">
         <template #default="scope">
           <dict-tag :options="game_types" :value="scope.row.gameType" />
         </template>
-      </el-table-column>
+      </el-table-column> -->
       <el-table-column label="当前版本" align="center" prop="version" />
       <el-table-column label="最近价格" align="center" prop="price" />
       <el-table-column label="开发商" align="center" prop="gameStudio" />
@@ -62,81 +62,79 @@
 </template>
 
 <script setup name="Info">
-import dayjs from 'dayjs';
-import { listGameInfo, delGameInfo, getGameInfo } from '@/api/game/info';
-import useTable from '@/hooks/useTable';
-import { reactive, computed, toRefs, ref } from 'vue';
-import DictTag from '@/components/DictTag/index.vue';
-import EditView from './components/edit.vue';
-import Preview from './components/preview.vue';
+import dayjs from 'dayjs'
+import { listGameInfo, delGameInfo, getGameInfo } from '@/api/game/info'
+import useTable from '@/hooks/useTable'
+import EditView from './components/edit.vue'
+import Preview from './components/preview.vue'
 
 const modules = {
   tableKey: 'gameId',
   name: '游戏信息'
-};
+}
 
-const queryRef = ref();
+const queryRef = ref()
 const queryParams = reactive({
   nameZh: '',
   nameEn: '',
   gameType: ''
-});
+})
 
-const gameInfoTable = useTable({ get: listGameInfo, delete: delGameInfo }, queryParams, queryRef);
+const gameInfoTable = useTable({ get: listGameInfo, delete: delGameInfo }, queryParams, queryRef)
 
 const tableData = computed(() => {
-  const list = gameInfoTable.state.list;
-  return list.map(item => ({
+  const list = gameInfoTable.state.list
+  return list.map((item) => ({
     ...item,
     createTime: item.createTime ? dayjs(item.createTime).format('YYYY-MM-DD HH:mm:ss') : '',
     updateTime: item.updateTime ? dayjs(item.updateTime).format('YYYY-MM-DD HH:mm:ss') : ''
-  }));
-});
+  }))
+})
 
 // 使用数据字典
-const { proxy } = getCurrentInstance();
-const { game_types } = proxy.useDict('game_type');
+const { proxy } = getCurrentInstance()
+const { game_types } = proxy.useDict('game_type')
 
-const showSearch = ref(true);
-const ids = ref([]);
-const single = ref(true);
-const multiple = ref(true);
+const showSearch = ref(true)
+const ids = ref([])
+const single = ref(true)
+const multiple = ref(true)
 
 const handleSelectionChange = (selection) => {
-  ids.value = selection.map((item) => item[modules.tableKey]);
-  single.value = selection.length != 1;
-  multiple.value = !selection.length;
-};
+  ids.value = selection.map((item) => item[modules.tableKey])
+  single.value = selection.length != 1
+  multiple.value = !selection.length
+}
 
 const handleDelete = (row) => {
-  const gameIds = row[modules.tableKey] || ids.value;
+  const gameIds = row[modules.tableKey] || ids.value
   ElMessageBox.confirm('您确认要删除该数据吗？', '删除提示', {
     confirmButtonText: '确定',
     cancelButtonText: '取消',
     type: 'warning'
   }).then(() => {
-    gameInfoTable.onDelete(gameIds);
-  });
-};
+    gameInfoTable.onDelete(gameIds)
+  })
+}
 
-const editViewRef = ref();
-const PreviewRef = ref();
+const editViewRef = ref()
+const PreviewRef = ref()
 
 const handlePreview = (row) => {
-  PreviewRef.value.handleOpen(row);
-};
+  PreviewRef.value.handleOpen(row)
+}
 
 const handleAdd = () => {
-  editViewRef.value.handleAdd();
-};
+  editViewRef.value.handleAdd()
+}
 
 const handleUpdate = (row) => {
-  editViewRef.value.handleUpdate(row);
-};
+  editViewRef.value.handleUpdate(row)
+}
 
 const handleEditSuccess = () => {
-  gameInfoTable.request();
-};
+  gameInfoTable.request()
+}
 </script>
 
 <style scoped></style>

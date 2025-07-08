463d0970540f4e555a3317c311660c81f169d564
crazystudent13 <1076535690@qq.com>
2025-07-08 23:40:08 +0800

    refactor(game-info): 重构游戏信息预览界面
    - 使用 el-descriptions 组件替代 el-form，提高可读性和美观度
    - 移除游戏类型字段（可选，根据需求恢复）
    - 优化布局，调整为单列显示
    - 添加自定义样式，提升界面美观

--- admin-vue3/src/views/game/info/components/preview.vue
+++ admin-vue3/src/views/game/info/components/preview.vue
@@ -1,81 +1,39 @@
 <template>
-  <el-drawer v-model="drawer.visible" :title="dialogTitle" size="600px" direction="rtl">
-    <el-form label-width="120px">
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="游戏中文名">
-            <el-input v-model="form.nameZh" disabled />
-          </el-form-item>
-        </el-col>
-      </el-row>
+  <el-drawer v-model="drawer.visible" :header-class="'drawer-title'" size="1000px" direction="rtl" @close="handleClose">
+    <template #title>
+      <h3>{{ drawer.title }}</h3>
+    </template>
 
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="游戏英文名">
-            <el-input v-model="form.nameEn" disabled />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="游戏类型">
-            <el-select v-model="form.gameType" disabled style="width: 100%">
-              <el-option v-for="dict in game_types" :key="dict.value" :label="dict.label" :value="dict.value" />
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="当前版本">
-            <el-input v-model="form.version" disabled />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="最近价格">
-            <el-input-number v-model="form.price" disabled style="width: 100%" />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="开发商">
-            <el-input v-model="form.gameStudio" disabled />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="24">
-          <el-form-item label="游戏简介">
-            <el-input v-model="form.desc" type="textarea" :rows="3" disabled />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="12">
-          <el-form-item label="创建时间">
-            <el-input v-model="form.createTime" disabled />
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="修改时间">
-            <el-input v-model="form.updateTime" disabled />
-          </el-form-item>
-        </el-col>
-      </el-row>
-    </el-form>
+    <el-tabs v-model="drawer.activeName" style="margin-top: 0px">
+      <el-tab-pane label="基本信息" name="first">
+        <el-descriptions :column="1" border>
+          <el-descriptions-item label="游戏中文名" label-class-name="desc-label">{{ form.nameZh }}</el-descriptions-item>
+          <el-descriptions-item label="游戏英文名" label-class-name="desc-label">{{ form.nameEn }}</el-descriptions-item>
+          <!-- <el-descriptions-item label="游戏类型" label-class-name="desc-label">
+            <el-tag size="small">{{ gameTypeLabel }}</el-tag>
+          </el-descriptions-item> -->
+          <el-descriptions-item label="当前版本" label-class-name="desc-label">{{ form.version }}</el-descriptions-item>
+          <el-descriptions-item label="最近价格" label-class-name="desc-label">{{ form.price }} 元</el-descriptions-item>
+          <el-descriptions-item label="开发商" label-class-name="desc-label">{{ form.gameStudio }}</el-descriptions-item>
+          <el-descriptions-item label="游戏简介" :span="3" label-class-name="desc-label">
+            {{ form.desc }}
+          </el-descriptions-item>
+          <el-descriptions-item label="创建时间" label-class-name="desc-label">
+            {{ form.createTime }}
+          </el-descriptions-item>
+          <el-descriptions-item label="修改时间" label-class-name="desc-label">
+            {{ form.updateTime }}
+          </el-descriptions-item>
+        </el-descriptions>
+      </el-tab-pane>
+    </el-tabs>
   </el-drawer>
 </template>
 
 <script setup>
+// const { proxy } = getCurrentInstance()
+// const game_types = useDict('game_type')
+
 const form = ref({
   gameId: null,
   nameZh: '',
@@ -91,12 +49,15 @@ const form = ref({
 
 const drawer = ref({
   visible: false,
+  activeName: 'first',
   title: '游戏信息详情'
 })
 
-// 使用数据字典
-const { proxy } = getCurrentInstance()
-const { game_types } = proxy.useDict('game_type')
+// // 计算属性获取游戏类型标签
+// const gameTypeLabel = computed(() => {
+//   const type = game_types.value.find((t) => t.value === form.value.gameType)
+//   return type ? type.label : '未知类型'
+// })
 
 // 打开预览对话框
 const handleOpen = (row) => {
@@ -121,12 +82,17 @@ defineExpose({
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .drawer-title {
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 0 20px 20px 0;
   font-weight: bold;
+}
+
+.desc-label {
+  width: 120px;
+  font-weight: 800;
 }
 </style>
